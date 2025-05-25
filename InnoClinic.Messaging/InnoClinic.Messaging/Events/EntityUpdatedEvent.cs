e473f83fa94eb35ad578a9c80ed66221edf51a84
IvanKurikin <ivan.kurykin@innowise.com>
2025-05-25 21:47:17 +0300

    add entity updated event class

---
+++ InnoClinic.Messaging/InnoClinic.Messaging/Events/EntityUpdatedEvent.cs
@@ -0,0 +1,9 @@
+﻿namespace InnoClinic.Messaging.Events;
+
+public record EntityUpdatedEvent<T>
+{
+    public Guid Id { get; init; }
+    public string? EntityType { get; init; } 
+    public DateTime UpdatedAt { get; init; }
+    public T? Payload { get; init; }
+}
